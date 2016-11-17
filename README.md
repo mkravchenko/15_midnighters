b9b4219e13a5290edec691ce02233ff1bdf59de4
mkravchenko <m.kravchenko18@gmail.com>
2016-11-17 18:15:40 +0200

    init code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # 15_midnighters
+
+This script prints users from site devman.com that commits their code between 00:00:00 and 06:00:00.
+
+# How to run:<br />
+1. Run the script:<br />
+py -3 seek_dev_nighters.py.py <br />
+
+#Results:
+Midnight users are:<br />
+Username 		    Timestamp 		        Timezone<br />
+МаксимМатейчик 		05:20:16.224881 		Europe/Moscow<br />
+МаксимМатейчик 		04:58:53.175077 		Europe/Moscow<br />
+МаксимМатейчик 		04:49:39.276821 		Europe/Moscow<br />
+timurgosu 		03:18:21.692720 		Europe/Moscow<br />
+timurgosu 		02:56:20 		Europe/Moscow<br />
+timurgosu 		02:47:48.009767 		Europe/Moscow<br />
+timurgosu 		01:27:12.691486 		Europe/Moscow<br />
+ЕвгенийВасильев 		00:37:35 		Europe/Moscow<br />
+oleg.smedyuk 		00:18:00 		Europe/Moscow<br />
+#...               ...                  ...
+beastrock 		01:08:05.288551 		Europe/Moscow<br />
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,52 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from pytz import timezone, utc
+import datetime as d
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+DEV_URL = 'http://devman.org/api/challenges/solution_attempts/'
+
+
+def load_attempts():
+    pages = get_number_of_pages()
+    for page in range(1, pages + 1):
+        payload = {"page": page}
+        response = requests.get(DEV_URL, params=payload).json()
+        for records in response['records']:
+            user_name = records['username']
+            timestamp = records['timestamp']
+            time_zone = records['timezone']
+            if timestamp and time_zone:
+                commit_time = utc.localize(datetime.utcfromtimestamp(timestamp)).astimezone(timezone(time_zone)).time()
+                is_midnighter = is_midnighter_current_user(commit_time)
+                if is_midnighter:
+                    yield {
+                        'username': user_name,
+                        'timestamp': commit_time,
+                        'timezone': time_zone,
+                    }
+
+
+def is_midnighter_current_user(time_z):
+    t1 = d.time(0, 0, 0)
+    t2 = d.time(6, 0, 0)
+    return True if t1 < time_z < t2 else False
+
+
+def get_number_of_pages():
+    response = requests.get(DEV_URL).json()
+    number_of_pages = response["number_of_pages"]
+    if not number_of_pages or number_of_pages is None:
+        exit("Number of pages can't be {}".format(number_of_pages))
+    return int(number_of_pages)
+
+
+def print_midnighters():
+    attempt = load_attempts()
+    print("Midnight users are:\n{0} \t\t{1} \t\t{2}".format('Username', 'Timestamp', 'Timezone'))
+    for params in attempt:
+        print("{0} \t\t{1} \t\t{2}".format(params['username'], params['timestamp'], params['timezone']))
+
 
 if __name__ == '__main__':
-  pass
+    print_midnighters()
